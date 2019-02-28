0b314b05b6450dfb8381f0633cd08a95911d82be
sucanthudu <sucanthudu@gmail.com>
2019-02-28 17:40:02 +0530

    created our first apiview

--- src/profiles_project/profiles_api/admin.py
+++ src/profiles_project/profiles_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# Register your models here with django admin
+admin.site.register(models.UserProfile)
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request, format=None):
+        """Returns a list of API view features"""
+
+        api=['get','post','put','del']
+
+        return Response({'message':api})
