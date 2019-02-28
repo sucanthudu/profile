41eef5d3f91a80940b3ea13b5bb6b25055a2050d
sucanthudu <sucanthudu@gmail.com>
2019-02-28 20:06:08 +0530

    created viewsets with basic crud ops

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -42,3 +43,51 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Deletes an object"""
         return Response({"method": "delete"})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test api viewset"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message"""
+
+        a_viewset = [
+            'User actions(list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS to routers',
+            'provides more functionality with less code'
+        ]
+        return Response({"message":a_viewset})
+
+    def create(self, request):
+        """create a new hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({"message":message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object by its ID"""
+
+        return Response({"http_method":"GET"})
+
+    def update(self, request, pk=None):
+        """update an object"""
+
+        return Response({"http_method":"PUT"})
+
+    def partial_update(self, request, pk=None):
+        """partially update an object"""
+
+        return Response({"http_method":"PATCH"})
+
+    def delete(self, request, pk=None):
+        """deletes an object"""
+
+        return Response({"http_method":"DELETE"})
